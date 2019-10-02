5dc8d68922e3be068c25d66f1f4c35a1c0392bfb
Richel Leung <>
2019-10-02 13:49:28 -0700

    feat(React Tutorial): Counter App
    Created Counter app with React
    
    [ICTRAINING-282]

---
+++ counter-app/src/components/navbar.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+
+//Stateless Functional Component
+const NavBar = ({ totalCounters }) => {
+  console.log("NavBar - Rendered");
+  return (
+    <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+      <a className="navbar-brand" href="#">
+        Navbar{" "}
+        <span className="badge badge-pill badge-secondary">
+          {totalCounters}
+        </span>
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarCollapse"
+        aria-controls="navbarCollapse"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+    </nav>
+  );
+};
+
+// class NavBar extends Component {
+//   render() {
+
+//   }
+// }
+
+export default NavBar;
